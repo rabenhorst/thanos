a6e6ed44d4e677c263dc273c242a3f0f3d1c9b12
Sebastian Rabenhorst <sebastian.rabenhorst@shopify.com>
2023-02-07 14:06:41 +0100

    Fixes to block fetcher and compactv2 modifiers

--- pkg/compactv2/modifiers.go
+++ pkg/compactv2/modifiers.go
@@ -374,7 +374,7 @@ func (d *RelabelModifier) Modify(_ index.StringIter, set storage.ChunkSeriesSet,
 
 		// The labels have to be copied because `relabel.Process` is now overwriting the original
 		// labels to same memory. This happens since Prometheus v2.39.0.
-		if processedLabels, keep := relabel.Process(lbls.Copy(), d.relabels...); !keep {
+		if processedLabels, _ := relabel.Process(lbls.Copy(), d.relabels...); len(processedLabels) == 0 {
 			// Special case: Delete whole series if no labels are present.
 			var (
 				minT int64 = math.MaxInt64
